fe4b3b83427bf7168ee62fdcfcf16f578dec756b
Bala <balasubramania@vientitydata.com>
2025-06-12 18:34:23 +0530

    eslint prettier setup

---
+++ eslint.config.cjs
@@ -0,0 +1,49 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      prettier: prettierPlugin
+    },
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      "no-duplicate-imports": "error",
+      "no-console": "warn",
+      "no-unreachable": "error",
+      "no-undef": "error",
+      "no-trailing-spaces": "error",
+      "no-unused-vars": "off",
+      "no-extra-semi": "error",
+      "no-nested-ternary": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "no-var": "error",
+      "jsx-quotes": ["warn", "prefer-double"],
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css';
+import styles from './app.module.scss';
 
-function App() {
-  return (
-    <div className="App">
-      hiii
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className={styles.app}>erfer</div>;
+};
 
 export default App;
